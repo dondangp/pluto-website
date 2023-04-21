6519b451801c4d0b40a64b5ca77fb41dfa2fccff
EthanDebnath <ethan.debnath@yahoo.com>
2023-04-21 16:21:13 -0500

    added navbar CSS code and assets

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {Article, Brand, CTA, Navbar} from './components'
 
 import {Features, Footer, Header, Intro, Mail} from './containers'
 
+import './App.css'
+
 const App = () => {
      return (
           <div className="App">
 
-               <div className='gradient__bg'>
+               <div className='header-bg'>
                     <Navbar />
                     <Header />
 
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import { RiMenu3Line, RiClloseLine} from 'react-icons/ri'
+import logo from '../../assets/plutologo.svg'
 import './navbar.css'
+
+// BEM -> Block Element Modifier
+// Block -> nav-bar
+// Element -> nav-bar__menu
+// Modifier -> nav-bar__menu--active
 
 const Navbar = () => {
      return (
-          <div>
-               Navbar
+          <div className='nav-bar'>
+               <div className='nav-bar-links'>
+                    <div className='nav-bar_logo'>
+                         <img src={logo} alt='logo' />
+                    </div>
+
+                    <div className='nav-bar-links-container'>
+                         <p><a href="#home"></a>Home</p>
+                         <p><a href="#abt"></a>About</p>
+                         <p><a href="#help"></a>Help</p>
+                         <p><a href="#money"></a>Premium</p>
+                    </div>
+
+                    <div className='nav-bar-links-signup'>
+                         <button type="button">Sign Up</button>
+                    </div>
+               </div>
           </div>
      )
 
