6365cae317d0aa32404592a8318effffe97c689a
Don Dang <dondang@Dons-MacBook-Pro.local>
2023-07-14 23:43:50 -0500

    added header svg for pluto header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './header.css'
-
+import logo from '../../assets/header_icon.svg'
 const Header = () => {
      return (
           <div className='header'>
                <div className='header-title'>
-                    <h1>This is plut.</h1>
+               <div className="pluto_icon"><img src={logo} /></div>
                </div>
                <div className='header-subtitle'>
                     <h2>connect. grow. explore</h2>
