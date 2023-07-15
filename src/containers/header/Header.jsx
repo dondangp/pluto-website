a736b0a2602441c0db7eb2b4dff43a7f6832d755
Don Dang <dondang@Dons-MacBook-Pro.local>
2023-07-14 23:59:18 -0500

    changed website title

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,19 +1,29 @@
-import React from 'react'
-import './header.css'
-import logo from '../../assets/header_icon.svg'
-const Header = () => {
-     return (
-          <div className='header'>
-               <div className='header-title'>
-               <div className="pluto_icon"><img src={logo} /></div>
-               </div>
-               <div className='header-subtitle'>
-                    <h2>connect. grow. explore</h2>
-               </div>
+import React, { useEffect } from 'react';
+import './header.css';
 
-          </div>
-     )
+function App() {
+  useEffect(() => {
+    document.title = "Pluto Learning";
+  }, []);
 
+  // Rest of your component code
+  return (
+    <Header />
+    // JSX elements
+  );
 }
 
-export default Header
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className='header-title'>
+        <h1>This is Plut</h1>
+      </div>
+      <div className='header-subtitle'>
+        <h2>connect. grow. explore</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
